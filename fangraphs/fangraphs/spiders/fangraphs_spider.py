47004eaafc64219e11aedadd587e3ea07bb96c0c
Andrew Whitaker <whitaker@cs.washington.edu>
2014-04-05 08:08:34 -0700

    Fix spider

--- fangraphs/fangraphs/spiders/fangraphs_spider.py
+++ fangraphs/fangraphs/spiders/fangraphs_spider.py
@@ -1,4 +1,7 @@
 from scrapy.spider import Spider
+from scrapy.selector import Selector
+
+from fangraphs.items import *
 
 class FanGraphsSpider(Spider):
     name = "fangraphs"
@@ -8,5 +11,49 @@ class FanGraphsSpider(Spider):
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        open(filename, 'wb').write(response.body)
+        sel = Selector(response)
+        cells =  sel.xpath("//td[@class='grid_line_regular' or @class='grid_line_break']")
+        it = iter(cells)
+
+        def next_string():
+            s = it.next().xpath('text()').extract()[0]
+            return s
+
+        def next_float():
+            s = next_string().strip()
+            if s:
+                return float(s)
+            else:
+                return 0.0
+
+        def next_link_text():
+            s = it.next().xpath('a/text()').extract()[0]
+            return s
+
+        out = []
+
+        while True:
+            try:
+                idx = next_float()
+                name = next_link_text()
+                batting = next_float()
+                base_running = next_float()
+                fielding = next_float()
+                positional = next_float()
+                offense = next_float()
+                defense = next_float()
+                league = next_float()
+                replacement = next_float()
+                rar = next_float()
+                war = next_float()
+                dollars = next_string()
+
+                print name, rar
+                item = FangraphsItem()
+                item['name'] = name
+                item['rar'] = rar
+                out.append(item)
+
+            except StopIteration:
+                return out
+
